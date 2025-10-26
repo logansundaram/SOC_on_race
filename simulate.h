0e8c934d846734a92b59cc2c9769c53526155b6c
Logan Sundaram <63442954+logansundaram@users.noreply.github.com>
2025-10-26 12:41:51 -0700

    makefile started and header file debug

--- simulate.h
+++ simulate.h
@@ -7,8 +7,6 @@
 #include <cmath>
 #include <vector>
 
-#include "simulate.cpp"
-
 double Calc_SOC(double Current, double delta_t);
 double Calc_V1(double prev_V1, double R1, double I_L, double C_1, double delta_t);
 double Calc_V2(double prev_V2, double R2, double I_L, double C_2, double delta_t);
